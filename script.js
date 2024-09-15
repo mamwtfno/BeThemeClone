1fc009c4cde28718421dd1e394497b817981b7f1
mamwtfno <renukamishra163@gmail.com>
2024-09-15 16:37:20 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('scroll', () => {
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 0) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled');
+    }
+});
